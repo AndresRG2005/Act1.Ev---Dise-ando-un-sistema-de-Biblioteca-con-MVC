fb87527b449a484383302c7195c431abf22fda4f
Andres <andresramosarinaga216@gmail.com>
2026-02-23 00:13:53 +0000

    Añado la clase libro con atributos y encapsulación

---
+++ src/Libro.java
@@ -0,0 +1,15 @@
+package biblioteca.modelo;
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+    private String editorial;
+    private Genero genero;
+    private int copiasTotales;
+    private int copiasDisponibles;
+    private EstadoLibro estado;
+    
+    }
+}
