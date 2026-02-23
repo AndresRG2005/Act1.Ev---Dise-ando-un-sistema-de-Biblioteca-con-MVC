91378dd1ee5c1fe40318ef0fe9d8d74645a7c5da
Andres <andresramosarinaga216@gmail.com>
2026-02-23 00:23:04 +0000

    Validacion y asignacion de valores

--- src/Libro.java
+++ src/Libro.java
@@ -11,8 +11,20 @@ public class Libro {
     private int copiasDisponibles;
     private EstadoLibro estado;
 
-   public Libro(String isbn, String titulo, String autor,
+    public Libro(String isbn, String titulo, String autor,
                  int anioPublicacion, String editorial,
                  Genero genero, int copiasTotales) {
+        if (isbn == null || isbn.isBlank()) {
+            throw new IllegalArgumentException("ISBN no puede ser vacío");
+        }
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+        this.editorial = editorial;
+        this.genero = genero;
+        this.copiasTotales = copiasTotales;
+        this.copiasDisponibles = copiasTotales;
+        this.estado = EstadoLibro.DISPONIBLE;
     }
 }
